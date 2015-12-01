e398b53807b2801a4d926b5d03d4013ad1256ec8
Justin Woodford <jewoodford@Justins-Necronomicon.local>
2015-12-01 10:38:19 -0600

    catching up through where exercise

--- album_seeder.sql
+++ album_seeder.sql
@@ -1,1 +1,10 @@
-album_seeder.sql
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 43.3, 'Pop'),
+	('AC/DC', 'Back in Black', 1980, 25.9, 'Hard rock'),
+	('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'Rock'),
+	('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B'),
+	('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard rock'),
+	('Eagles', 'Their Greatest Hits', 1976, 32.2, 'Rock'),
+	('Bee Gees', 'Saturday Night Fever', 1977, 19.0, 'Disco'),
+	('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock')
--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	artist VARCHAR(20),
-	name VARCHAR(25),
-	release_date YEAR,
-	sales INT,
-	genre VARCHAR(10),
+	artist VARCHAR(20) NOT NULL,
+	name VARCHAR(25) NOT NULL,
+	release_date INT(4) NOT NULL,
+	sales DECIMAL(4, 1) NOT NULL,
+	genre VARCHAR(10) NOT NULL,
 	PRIMARY KEY (id)
 );
