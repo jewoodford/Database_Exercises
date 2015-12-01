2ac1466413a9415dba036520aca32d370cbd7b6e
Justin Woodford <jewoodford@Justins-Necronomicon.local>
2015-12-01 10:49:20 -0600

    fourth step of where cont exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,5 +1,5 @@
-SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
 SELECT * FROM employees WHERE hire_date LIKE '199%';
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
 SELECT * FROM employees WHERE last_name LIKE '%q%';
